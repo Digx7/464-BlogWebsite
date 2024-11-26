4d4a6fca70123fd2d5739afaa9dedd0beac1e04b
Digx7 <37136301+Digx7@users.noreply.github.com>
2024-11-26 14:08:54 -0800

    database now builds if empty

--- sisyphus-blog-backend/app.js
+++ sisyphus-blog-backend/app.js
@@ -82,29 +82,133 @@ function createDataBase() {
 
 function createTables(newdb) {
   console.log(`Running Create Tables`);
+
+  console.log(`Creating Users`);
+  newdb.exec(`
+    CREATE TABLE users (
+      user_id INTEGER PRIMARY KEY NOT NULL,
+      username TEXT NOT NULL,
+      profile_pic BLOB,
+      password_hash TEXT NOT NULL
+    );
+    INSERT INTO users
+      VALUES (1, 'Digx7', NULL, 'jR;e~HWtPQKiFkWco.cY)=wE^)=+,#8Du=A60H[Coli1Pn4J7Z'),
+              (2, 'Masher2', NULL, 'pP}iMWySTLRadUutBMez(Mv(eXAL._r[wnTM;y2OXroDmCV=1'),
+              (3, 'Oceanstuck', NULL, 'gwTdi!Sby5JQXe6vUZSu.HEN[$@!$X#%rX-#X+AOji~k2y~16'),
+              (4, 'Jxxmimi', NULL, 'r-Y+%UM9#z$1N~zxkCOm54Ch~C@noKtq8uTljE4xQs3q[CO06K');
+  `, (err) => {
+    if (err) {
+      console.log("Getting Error " + err);
+      exit(1);
+    }
+  });
+
+  console.log(`Creating Blogs`);
   newdb.exec(`
     CREATE TABLE blogs (
       blog_id INTEGER PRIMARY KEY NOT NULL,
       title TEXT NOT NULL,
-      author TEXT NOT NULL
+      author_id INTEGER NOT NULL,
+      date_published TEXT NOT NULL,
+      date_last_updated TEXT NOT NULL,
+      content TEXT NOT NULL,
+      FOREIGN KEY (author_id) REFERENCES users(user_id) ON DELETE SET DEFAULT
     );
-    INSERT INTO BLOGS (blog_id, title, author)
-      values (1, 'Blog 1', 'Everette'),
-            (2, 'Blog 2', 'Shelby')`, () => {
-      runQueries(newdb);
-    });
+    INSERT INTO blogs
+      VALUES (1, 'Top 10 Ways to Cheat On A Final Project', 1, datetime('now'), datetime('now'), '<!DOCTYPE html>
+              <html lang="en">
+              <head>
+                <meta charset="utf-8">
+                <title>Top 10 Ways to Cheat On A Final Project</title>
+              </head>
+              <body>
+                <p>DONT</p>
+              </body>
+              </html>');
+    `, (err) => {
+      if (err) {
+        console.log("Getting Error " + err);
+        exit(1);
+      }
+  });
+
+  console.log(`Creating Comments`);
+  newdb.exec(`
+    CREATE TABLE comments (
+      comment_id INTEGER PRIMARY KEY NOT NULL,
+      user_id INTEGER NOT NULL,
+      blog_id INTEGER NOT NULL,
+      date_posted TEXT NOT NULL,
+      content TEXT NOT NULL,
+      FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE,
+      FOREIGN KEY (blog_id) REFERENCES blogs(blog_id) ON DELETE CASCADE
+    );
+    INSERT INTO comments
+      VALUES (1, 1, 1, '2024-10-10 13:30:30:003', 'First');
+    `, (err) => {
+      if (err) {
+        console.log("Getting Error " + err);
+        exit(1);
+      }
+  });
+
+  runQueries(newdb);
+
+
+  // newdb.exec(`
+  //   CREATE TABLE users (
+  //     user_id INTEGER PRIMARY KEY NOT NULL,
+  //     username TEXT NOT NULL,
+  //     profile_pic BLOB,
+  //     password_hash TEXT NOT NULL
+  //   );
+  //   CREATE TABLE blogs (
+  //     blog_id INTEGER PRIMARY KEY NOT NULL,
+  //     title TEXT NOT NULL,
+  //     author_id INTEGER NOT NULL,
+  //     date_published TEXT NOT NULL,
+  //     date_last_updated TEXT NOT NULL,
+  //     content TEXT NOT NULL,
+  //     FOREIGN KEY (author_id) REFERENCES users(user_id) ON DELETE SET DEFAULT
+  //   );
+  //   CREATE TABLE comments (
+  //     comment_id INTEGER PRIMARY KEY NOT NULL,
+  //     user_id INTEGER NOT NULL,
+  //     blog_id INTEGER NOT NULL,
+  //     date_posted TEXT NOT NULL,
+  //     content TEXT NOT NULL
+  //     FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
+  //     FOREIGN KEY (blog_id) REFERENCES blogs(blog_id) ON DELETE CASCADE
+  //   );
+  //   INSERT INTO users
+  //     VALUES (NULL, 'Digx7', NULL, 'jR;e~HWtPQKiFkWco.cY)=wE^)=+,#8Du=A60H[Coli1Pn4J7Z'),
+  //             (NULL, 'Masher2', NULL, 'pP}iMWySTLRadUutBMez(Mv(eXAL._r[wnTM;y2OXroDmCV=1'),
+  //             (NULL, 'Oceanstuck', NULL, 'gwTdi!Sby5JQXe6vUZSu.HEN[$@!$X#%rX-#X+AOji~k2y~16'),
+  //             (NULL, 'Jxxmimi', NULL, 'r-Y+%UM9#z$1N~zxkCOm54Ch~C@noKtq8uTljE4xQs3q[CO06K');
+  //   INSERT INTO blogs
+  //     VALUES (NULL, 'Top 10 Ways to Cheat On A Final Project', 0, '2024-10-10 13:30:30:003','2024-10-10 13:30:30:003', '<!DOCTYPE html>
+  //             <html lang="en">
+  //             <head>
+  //               <meta charset="utf-8">
+  //               <title>Top 10 Ways to Cheat On A Final Project</title>
+  //             </head>
+  //             <body>
+  //               <p>DONT</p>
+  //             </body>
+  //             </html>');
+  //   INSERT INTO comments
+  //     VALUES (NULL, 0, 0, '2024-10-10 13:30:30:003', 'First');
+    
+  //   `, () => {
+  //     runQueries(newdb);
+  //   });
 }
 
 function runQueries(db) {
-  db.all(`SELECT * FROM blogs WHERE blog_id = ?`, 2, (err, rows) => {
-    // console.log(rows)
+  // db.all(`SELECT * FROM comments WHERE blog_id = ?`, 1, (err, rows) => {
     
-    rows.forEach(row => {
-      console.log (row.title + "\t" +
-        row.author + "\t" +
-        row.data_published + "\t" +
-        row.data_last_updated + "\t" +
-        row.content);
-    });
-  });
+  //   rows.forEach(row => {
+  //     console.log (row.content);
+  //   });
+  // });
 }
