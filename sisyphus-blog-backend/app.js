b02a7fec425f9d58667d6c67952c868d84ed36ca
Digx7 <37136301+Digx7@users.noreply.github.com>
2024-11-26 12:46:29 -0800

    Got a very basic SQLite database working

--- sisyphus-blog-backend/app.js
+++ sisyphus-blog-backend/app.js
@@ -3,6 +3,7 @@ const cors = require('cors'); // Enable CORS
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
 
+
 // Load environment variables from .env file
 dotenv.config();
 
@@ -26,6 +27,18 @@ let blogs = [
   { id: 1, title: 'First Blog Post', content: 'This is the content of the first blog post.' },
   { id: 2, title: 'Second Blog Post', content: 'This is the content of the second blog post.' }
 ];
+
+// Connecting to Database
+var sqlite3 = require('sqlite3');
+let db = new sqlite3.Database('./db/main.db', sqlite3.OPEN_READWRITE, (err) => {
+  if (err && err.code == "SQLITE_CANTOPEN") {
+    createDataBase();
+  } else if (err) {
+    console.log("Getting error " + err);
+  } else {
+    runQueries(db);
+  }
+});
 
 // Blog routes for testing purposes
 app.get('/api/blogs', (req, res) => {
@@ -52,3 +65,46 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+
+
+
+
+function createDataBase() {
+  var newdb = new sqlite3.Database('./db/main.db', (err) => {
+    if (err) {
+      console.log("Getting error" + err);
+      exit(1);
+    }
+    createTables(newdb);
+  });
+}
+
+function createTables(newdb) {
+  console.log(`Running Create Tables`);
+  newdb.exec(`
+    CREATE TABLE blogs (
+      blog_id INTEGER PRIMARY KEY NOT NULL,
+      title TEXT NOT NULL,
+      author TEXT NOT NULL
+    );
+    INSERT INTO BLOGS (blog_id, title, author)
+      values (1, 'Blog 1', 'Everette'),
+            (2, 'Blog 2', 'Shelby')`, () => {
+      runQueries(newdb);
+    });
+}
+
+function runQueries(db) {
+  db.all(`SELECT * FROM blogs WHERE blog_id = ?`, 2, (err, rows) => {
+    // console.log(rows)
+    
+    rows.forEach(row => {
+      console.log (row.title + "\t" +
+        row.author + "\t" +
+        row.data_published + "\t" +
+        row.data_last_updated + "\t" +
+        row.content);
+    });
+  });
+}
